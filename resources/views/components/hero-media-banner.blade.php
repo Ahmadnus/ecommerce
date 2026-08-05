0eaa7c12644d1fe1a31ffd42cc50512806555b79
Ahmadnus <ahmad.mog2091@gmail.com>
2026-08-05 20:06:32 +0300

    Further shrink hero video height and width on mobile

--- resources/views/components/hero-media-banner.blade.php
+++ resources/views/components/hero-media-banner.blade.php
@@ -50,7 +50,7 @@
 
     $containerClass = $contained
         ? 'banner-container block w-full max-w-sm md:max-w-md mx-auto my-6 md:my-10 rounded-2xl overflow-hidden relative aspect-[9/16] shadow-lg'
-        : 'banner-container block w-full max-w-4xl mx-auto px-2 sm:px-6 my-2 md:my-6 aspect-video max-h-[38vh] md:max-h-[calc(60vh-150px)] overflow-hidden relative';
+        : 'banner-container block w-[85%] max-w-2xl mx-auto px-2 sm:px-6 my-2 md:my-6 aspect-video max-h-[26vh] md:max-h-[calc(60vh-150px)] overflow-hidden relative';
 
     $mediaClass = $contained
         ? 'w-full h-full object-cover'
